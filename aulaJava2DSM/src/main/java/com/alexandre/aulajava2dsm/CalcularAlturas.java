3cffa1dea3bcc8c876f96ebb515cdaca1c183fb2
XandyGomes <x3.computadores@hotmail.com>
2025-03-14 22:11:15 -0300

    Aula.14.03.2025

--- aulaJava2DSM/src/main/java/com/alexandre/aulajava2dsm/CalcularAlturas.java
+++ aulaJava2DSM/src/main/java/com/alexandre/aulajava2dsm/CalcularAlturas.java
@@ -11,22 +11,49 @@ import java.util.Scanner;
  * @author Alexandre
  */
 public class CalcularAlturas {
+
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-       
+
         double maiorAltura = 0.0;
-        double menorAltura = 999.9;
+        double menorAltura = 99.0;
         double somaAlturasHomens = 0.0;
         int numeroMulheres = 0;
         int numeroHomens = 0;
         double altura = 0;
+        double mediaAlturasHomens = 0.0;
         String sexo;
-        
-        for(int i=1; i<=5; i++){
-            
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Digite a altura da " + i + "ª pessoa: ");
+            altura = input.nextDouble();
+
+            System.out.print("Digite o sexo da " + i + "ª *M/F: ");
+            sexo = input.next();
+
+            if (altura > maiorAltura) {
+                maiorAltura = altura;
+            }
+            if (altura < menorAltura) {
+                menorAltura = altura;
+            }
+            if (sexo.equals("M")) {
+                somaAlturasHomens += altura;
+                numeroHomens++;
+            } else {
+                numeroMulheres++;
+            }
+
         }
-        
+
+        mediaAlturasHomens = somaAlturasHomens / numeroHomens;
+
+        System.out.println("Maior altura: " + maiorAltura);
+        System.out.println("Menor altura: " + menorAltura);
+        System.out.println("Média de altura dos homens: " + mediaAlturasHomens);
+        System.out.println("Número de mulheres: " + numeroMulheres);
+
     }
-    
+
 }
